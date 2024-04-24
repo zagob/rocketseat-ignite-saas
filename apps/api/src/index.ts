d71bfdd17fdeaae021d84cb013bfb6ba82535a04
zagob <matheusbestana@gmail.com>
2024-04-24 16:45:25 -0300

    initial project + config lib + init casl

---
+++ apps/api/src/index.ts
@@ -0,0 +1,6 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
